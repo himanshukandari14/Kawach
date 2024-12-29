a9952b1e58994fadb6057b5325396705937871b0
Vansh Tyagi <vanshjii021@gmail.com>
2024-12-29 11:51:03 +0530

    open port

--- server/index.js
+++ server/index.js
@@ -84,7 +84,7 @@ app.post('/upload', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
